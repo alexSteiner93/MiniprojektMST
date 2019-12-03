9a2e2773e480b2f298e627b9cc0048765e97de55
Alexander Steiner <a.steiner@hotmail.de>
2019-12-03 09:25:50 +0100

    created context

--- AutoReservation.BusinessLayer/AutoManager.cs
+++ AutoReservation.BusinessLayer/AutoManager.cs
@@ -6,11 +6,11 @@ namespace AutoReservation.BusinessLayer
     public class AutoManager
         : ManagerBase
     {
-        // Example
-        //public async Task<List<Auto>> GetAll()
-        //{
-        //    using AutoReservationContext context = new AutoReservationContext();
-        //    return await context.Autos.ToListAsync();
-        //}
+     
+        public async Task<List<Auto>> GetAll()
+        {
+          using AutoReservationContext context = new AutoReservationContext();
+           return await context.Autos.ToListAsync();
+        }
     }
 }
--- AutoReservation.Dal/AutoReservationContext.cs
+++ AutoReservation.Dal/AutoReservationContext.cs
@@ -1,8 +1,34 @@
-﻿namespace AutoReservation.Dal
+﻿using System.Configuration;
+using AutoReservation.Dal.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace AutoReservation.Dal
 {
     public class AutoReservationContext
-        : AutoReservationContextBase
+        : AutoReservationContextBase :DbContext
     {
-      
+
+        public DbSet<Auto> Autos { get; set; }
+
+        public DbSet<Kunde> Kunden { get; set; }
+
+        public DbSet<Reservation> Reservationen { get; set; }
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Auto>()
+            .ToTable("Auto", "dbo")
+            .HasDiscriminator<int>("AutoKlasse")
+            .HasValue<LuxusklasseAuto>(0)
+            .HasValue<MittelklasseAuto>(1)
+            .HasValue<StandardAuto>(2)
+            ;
+
+        modelBuilder.Entity<Kunde>().ToTable("Kunde", schema: "dbo");
+        modelBuilder.Entity<Reservation>().ToTable("Reservation", schema: "dbo");
     }
 }
