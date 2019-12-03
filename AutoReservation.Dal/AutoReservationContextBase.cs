d3d6b8c0f7488ce5fa2a553e47109b1d54a44556
Alexander Steiner <a.steiner@hotmail.de>
2019-12-03 08:32:13 +0100

    * appsettings.json: add files
    * xunit.runner.json:
    * Program.cs:
    * Startup.cs:
    * Managers.cd:
    * DtoConverter.cs:
    * AutoManager.cs:
    * ManagerBase.cs:
    * AutoReservationContext.cs:
    * KundeManager.cs:
    * AutoReservationContextBase.cs:
    * ReservationManager.cs:
    * AutoUpdateTest.cs:
    * KundeUpdateTest.cs:
    * AutoServiceTests.cs:
    * KundeServiceTests.cs:
    * ReservationUpdateTest.cs:
    * ServiceTestBase.cs:
    * ReservationServiceTests.cs:
    * ServiceTestFixture.cs:
    * ReservationDateRangeTest.cs:
    * ReservationAvailabilityTest.cs:

---
+++ AutoReservation.Dal/AutoReservationContextBase.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AutoReservation.Dal
+{
+    /// <summary>
+    /// This class contains static elements for logging / configuration.
+    /// 
+    /// WARNING:
+    /// Please DO NOT implement your database context classes like this in production code.
+    /// Here, we use static implementations since Dependency Injection is not covered
+    /// in this module.
+    /// </summary>
+    public abstract class AutoReservationContextBase
+        : DbContext
+    {
+        protected static readonly ILoggerFactory Logger;
+        protected static readonly IConfiguration Configuration;
+
+        static AutoReservationContextBase()
+        {
+            Logger = LoggerFactory.Create(builder =>
+            {
+                builder.AddConsole().SetMinimumLevel(LogLevel.Information);
+            });
+
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(
+                    "appsettings.json", 
+                    optional: true,
+                    reloadOnChange: true
+                )
+                .Build();           
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .EnableSensitiveDataLogging()
+                    .UseLoggerFactory(Logger)
+                    //.UseInMemoryDatabase("AutoReservation");
+                    .UseSqlServer(Configuration.GetConnectionString("AutoReservation"));
+            }
+        }
+    }
+}
